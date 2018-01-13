6e3ff6c45ca9f6b8c29c25ee9aab47e6c8e3e095
legiaquy <legiaquy@gmail.com>
2018-01-13 11:29:15 +0700

    [fixed] but my code might not be correct

--- 04.practical.work.fork.exec.c
+++ 04.practical.work.fork.exec.c
@@ -1,6 +1,21 @@
-gefit prog.c
 
 #include <stdio.h>
-int main(){
+#include <unistd.h>
 
+int main(){
+    printf ("Main before fork()\n");
+    int pid1 = fork();
+    if (pid1 == 0) {
+        printf ("launching ps -ef\n");
+        char *args[]= { "/bin/ps", "-ef" , NULL};
+        execvp("/bin/ps", args);
+    }
+    int pid2 = fork();
+    if (pid2 == 0) {
+        printf ("launching free -h\n");
+        char *args[]= { "free", "-h", NULL};
+        execvp("free", args);
+    }
+    else printf ("\nParent of ps :%d. Parent of free :%d\n",pid1,pid2);
+    return 0;
 }
