96fe5be54b6b30fef9a8c3b0ec4fced5d7a97d1d
legiaquy <legiaquy@gmail.com>
2018-01-19 22:39:54 +0700

    fix printf

--- 04.practical.work.fork.exec.c
+++ 04.practical.work.fork.exec.c
@@ -5,6 +5,7 @@
 int main(){
     printf ("Main before fork()\n");
     int pid1 = fork();
+    
     if (pid1 == 0) {
         printf ("launching ps -ef\n");
         char *args[]= { "/bin/ps", "-ef" , NULL};
@@ -12,10 +13,11 @@ int main(){
     }
     int pid2 = fork();
     if (pid2 == 0) {
-        printf ("launching free -h\n");
-        char *args[]= { "free", "-h", NULL};
-        execvp("free", args);
+	printf ("launching free -h\n");
+	char *args2[]={ "free","-h",NULL };
+	execvp("free",args2);
     }
-    else printf ("\nParent of ps :%d. Parent of free :%d\n",pid1,pid2);
+	else printf ("Child contains ps: %d, Child contains free: %d\n",pid1,pid2);
+    
     return 0;
 }
